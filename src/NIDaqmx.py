b2485cfdf84696ea3b1c10ae031897b215d9c63c
CrabAnderson <crab85193@gmail.com>
2022-08-22 10:54:31 +0900

    update: Device name can be changed and add exception handling

--- src/NIDaqmx.py
+++ src/NIDaqmx.py
@@ -2,21 +2,36 @@ import nidaqmx
 from nidaqmx.constants import (LineGrouping)
 
 class NIDaqmx:
-    def __init__(self,dev_name):
-        self.dev = dev_name
+    def __init__(self):
+        self.dev = None
 
+
+    def setDevName(self,dev_name):
+        self.dev = dev_name
+    
+    
     def getAIData(self,port):
-        with nidaqmx.Task() as task:
-            task.ai_channels.add_ai_voltage_chan(self.dev + "/" + port)
-            return task.read(number_of_samples_per_channel=1)
-        # print(self.port)
+        try:
+            with nidaqmx.Task() as task:
+                task.ai_channels.add_ai_voltage_chan(self.dev + "/" + port)
+                return task.read(number_of_samples_per_channel=1)
+        except nidaqmx.errors.DaqError:
+            return [0]
+
         
     def setAOData(self,port,data):
-        with nidaqmx.Task() as task:
-            task.ao_channels.add_ao_voltage_chan(self.dev + "/" + port)
-            task.write([data], auto_start=True)
-            
+        try:
+            with nidaqmx.Task() as task:
+                task.ao_channels.add_ao_voltage_chan(self.dev + "/" + port)
+                task.write([data], auto_start=True)    
+        except nidaqmx.errors.DaqError:
+            pass
+        
+        
     def setDOData(self,port,lineCh,data):
-        with nidaqmx.Task() as task:
-            task.do_channels.add_do_chan(self.dev + "/" + port + "/" + lineCh, line_grouping=LineGrouping.CHAN_PER_LINE)
-            task.write([data])
+        try:
+            with nidaqmx.Task() as task:
+                task.do_channels.add_do_chan(self.dev + "/" + port + "/" + lineCh, line_grouping=LineGrouping.CHAN_PER_LINE)
+                task.write([data])
+        except nidaqmx.errors.DaqError:
+            pass
